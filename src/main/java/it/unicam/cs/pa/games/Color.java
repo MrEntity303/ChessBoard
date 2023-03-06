687f9799f3bdbb1f85091d3ee259843f2f7100ca
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-03-06 11:00:40 +0100

    [ADD] enumeration for typePiece and ColorPiece

---
+++ src/main/java/it/unicam/cs/pa/games/Color.java
@@ -0,0 +1,5 @@
+package it.unicam.cs.pa.games;
+
+public enum Color {
+    WHITE, BLACK
+}
---
+++ src/main/java/it/unicam/cs/pa/games/chess/PieceChessType.java
@@ -0,0 +1,5 @@
+package it.unicam.cs.pa.games.chess;
+
+public enum PieceChessType {
+    KING/*RE*/, QUEEN/*REGINA*/, ROOK/*TORRE*/, BISHOP/*ALFIERE*/, KNIGHT/*CAVALLO*/, PAWN/*PEDONE*/
+}
