41472ecc821d7b92ad00d6b8c2c016f23858d93d
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-03-06 11:01:21 +0100

    [UPDATE] abstract class for define engineGame

--- src/main/java/it/unicam/cs/pa/games/Game.java
+++ src/main/java/it/unicam/cs/pa/games/Game.java
@@ -1,4 +1,39 @@
 package it.unicam.cs.pa.games;
 
+import it.unicam.cs.pa.games.chess.PieceChess;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 public abstract class Game {
+    @Getter(AccessLevel.PRIVATE)
+    @Setter(AccessLevel.PRIVATE)
+    private int countTurns;
+        //template method
+        public final void play() {
+            initialize();
+            while (!endOfGame()) {
+                makeTurn();
+            }
+            printResult();
+        }
+
+        //hook methods - possono essere ridefiniti dalle sottoclassi
+        protected void initialize() {
+            //this.setCountTurns(0);
+            System.out.println("Inizializzazione del gioco");
+        }
+
+        protected abstract void makeTurn();
+
+        protected boolean endOfGame() {
+            //this.setCountTurns(this.getCountTurns() + 1);
+            return false;//TODO aggiungere condizione di fine gioco
+        }
+
+    protected abstract boolean validator(PieceChess pieceChess, int x, int y);
+
+    protected void printResult(){
+//        System.out.println("Il gioco è finito, turni passati: " + (this.getCountTurns()-1));
+    }
 }
