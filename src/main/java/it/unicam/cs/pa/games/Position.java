d5516280d5bf4e3af50f550b521cfc013e29e1f4
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-03-09 22:21:00 +0100

    [UPDATE] removed control condition on constructor

--- src/main/java/it/unicam/cs/pa/games/Position.java
+++ src/main/java/it/unicam/cs/pa/games/Position.java
@@ -1,8 +1,19 @@
 package it.unicam.cs.pa.games;
 
+import lombok.Setter;
+
+@Setter
 public record Position(int x, int y){
-    public Position{
-        if(x < 0 || y < 0)
-            throw new IllegalArgumentException("Position must be positive");
+//    public Position{
+////        if(x < 0 || y < 0)
+////            System.err.println("Posizione x: "+x+" y: "+y+" non valida");
+//    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Position p){
+            return p.x == this.x && p.y == this.y;
+        }
+        return false;
     }
 }
