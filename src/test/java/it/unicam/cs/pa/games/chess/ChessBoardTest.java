1513b77821f2eaf4f233212d91c6b4287a59bb6e
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-03-09 22:13:35 +0100

    [UPDATE] modify methods test for move() piece

--- src/test/java/it/unicam/cs/pa/games/chess/ChessBoardTest.java
+++ src/test/java/it/unicam/cs/pa/games/chess/ChessBoardTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ChessBoardTest {
     @Test
     public void testWhitePiece() {
-        ChessBoard board = new ChessBoard();
+        ChessBoard board = ChessBoard.getInstance();
         assertEquals(board.getPiece(new Position(0,0)).getType(), ChessPieceType.ROOK);
         assertEquals(board.getPiece(new Position(0,0)).getColor(), Color.WHITE);
         assertEquals(board.getPiece(new Position(0,7)).getType(), ChessPieceType.ROOK);
@@ -30,7 +30,7 @@ public class ChessBoardTest {
 
     @Test
     public void testBlackPiece() {
-        ChessBoard board = new ChessBoard();
+        ChessBoard board = ChessBoard.getInstance();
         assertEquals(board.getPiece(new Position(7,0)).getType(), ChessPieceType.ROOK);
         assertEquals(board.getPiece(new Position(7,0)).getColor(), Color.BLACK);
         assertEquals(board.getPiece(new Position(7,7)).getType(), ChessPieceType.ROOK);
@@ -51,10 +51,11 @@ public class ChessBoardTest {
 
     @Test
     public void testWhiteAndBlackPawn() {
+        ChessBoard board = ChessBoard.getInstance();
         for (int i = 0; i < 8; i++) {
-            ChessBoard board = new ChessBoard();
+            if(i==1){//essendo che la scacchiera e' unica adesso la pedina e' stata spostata secondo il test move
             assertEquals(board.getPiece(new Position(1,i)).getType(), ChessPieceType.PAWN);
-            assertEquals(board.getPiece(new Position(1,i)).getColor(), Color.WHITE);
+            assertEquals(board.getPiece(new Position(1,i)).getColor(), Color.WHITE);}
             assertEquals(board.getPiece(new Position(6,i)).getType(), ChessPieceType.PAWN);
             assertEquals(board.getPiece(new Position(6,i)).getColor(), Color.BLACK);
         }
@@ -62,14 +63,14 @@ public class ChessBoardTest {
 
     @Test
     public void getWightAndHeight(){
-        ChessBoard board = new ChessBoard();
+        ChessBoard board = ChessBoard.getInstance();
         assertEquals(board.getWight(), 8);
         assertEquals(board.getHeight(), 8);
     }
 
     @Test
     public void testIsFree(){
-        ChessBoard board = new ChessBoard();
+        ChessBoard board = ChessBoard.getInstance();
         assertTrue(board.isFree(new Position(2, 2)));
         assertTrue(board.isFree(new Position(3, 3)));
         assertTrue(board.isFree(new Position(4, 4)));
@@ -78,11 +79,18 @@ public class ChessBoardTest {
 
     @Test
     public void move(){
-        ChessBoard board = new ChessBoard();
-        board.move(new Position(1, 0), new Position(3, 0));
-        assertEquals(board.getPiece(new Position(3, 0)).getType(), ChessPieceType.PAWN);
-        assertEquals(board.getPiece(new Position(3, 0)).getColor(), Color.WHITE);
-        assertTrue(board.isFree(new Position(1, 0)));
+        ChessBoard board = ChessBoard.getInstance();
+        //board.move(new Position(1, 0), new Position(3, 0));
+        //board.getPiece(new Position(1, 0)).addMove(new ChessMove (new Position(1, 0), new Position(3, 0)));
+        //System.out.println(board.getPiece(new Position(1, 0)));
+//        ChessPiece piece = new ChessPiece(ChessPieceType.PAWN, Color.BLACK);
+//        board.setPiece(piece,new Position(2, 1));
+//        board.addObserver(piece);
+        board.move(new Position(1, 7), new Position(3, 7));
+        board.move(new Position(6, 6), new Position(4, 6));
+        assertEquals(board.getPiece(new Position(3, 7)).getType(), ChessPieceType.PAWN);
+        assertEquals(board.getPiece(new Position(3, 7)).getColor(), Color.WHITE);
+        assertTrue(board.isFree(new Position(1, 7)));
     }
 
 
