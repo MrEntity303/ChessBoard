0a90b456486ab37c9d5aaf1f8106ef11ec567095
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-03-09 22:16:24 +0100

    [UPDATE] adapt class for use a Singleton design

--- src/main/java/it/unicam/cs/pa/games/chess/ChessGame.java
+++ src/main/java/it/unicam/cs/pa/games/chess/ChessGame.java
@@ -4,7 +4,7 @@ import it.unicam.cs.pa.games.Game;
 import it.unicam.cs.pa.games.Position;
 
 public class ChessGame extends Game {
-    private final ChessBoard board = new ChessBoard();
+    private final ChessBoard board = ChessBoard.getInstance();
 
     @Override
     protected void initialize() {
@@ -23,47 +23,47 @@ public class ChessGame extends Game {
         return false;
     }
 
-    @Override
-    protected boolean validator(ChessPiece chessPiece, Position origin, Position destination) {
-        return switch (chessPiece.getType()) {
-            case KING -> kingValidator(chessPiece, origin, destination);
-            case QUEEN -> queenValidator(chessPiece, origin, destination);
-            case ROOK -> rookValidator(chessPiece, origin, destination);
-            case BISHOP -> bishopValidator(chessPiece, origin, destination);
-            case KNIGHT -> knightValidator(chessPiece, origin, destination);
-            case PAWN -> pawnValidator(chessPiece, origin, destination);
-        };
-    }
-
-    private boolean pawnValidator(ChessPiece chessPiece, Position origin, Position destination){
-        //TODO: implement
-        return false;
-    }
-
-    private boolean knightValidator(ChessPiece chessPiece, Position origin, Position destination) {
-        //TODO: implement
-        return false;
-    }
-
-    private boolean bishopValidator(ChessPiece chessPiece, Position origin, Position destination) {
-        //TODO: implement
-        return false;
-    }
-
-    private boolean rookValidator(ChessPiece chessPiece, Position origin, Position destination) {
-        //TODO: implement
-        return false;
-    }
-
-    private boolean queenValidator(ChessPiece chessPiece, Position origin, Position destination) {
-        //TODO: implement
-        return false;
-    }
-
-    private boolean kingValidator(ChessPiece chessPiece, Position origin, Position destination) {
-        //TODO: implement
-        return false;
-    }
+//    @Override
+//    protected boolean validator(ChessPiece chessPiece, Position origin, Position destination) {
+//        return switch (chessPiece.getType()) {
+//            case KING -> kingValidator(chessPiece, origin, destination);
+//            case QUEEN -> queenValidator(chessPiece, origin, destination);
+//            case ROOK -> rookValidator(chessPiece, origin, destination);
+//            case BISHOP -> bishopValidator(chessPiece, origin, destination);
+//            case KNIGHT -> knightValidator(chessPiece, origin, destination);
+//            case PAWN -> pawnValidator(chessPiece, origin, destination);
+//        };
+//    }
+//
+//    private boolean pawnValidator(ChessPiece chessPiece, Position origin, Position destination){
+//        //TODO: implement
+//        return false;
+//    }
+//
+//    private boolean knightValidator(ChessPiece chessPiece, Position origin, Position destination) {
+//        //TODO: implement
+//        return false;
+//    }
+//
+//    private boolean bishopValidator(ChessPiece chessPiece, Position origin, Position destination) {
+//        //TODO: implement
+//        return false;
+//    }
+//
+//    private boolean rookValidator(ChessPiece chessPiece, Position origin, Position destination) {
+//        //TODO: implement
+//        return false;
+//    }
+//
+//    private boolean queenValidator(ChessPiece chessPiece, Position origin, Position destination) {
+//        //TODO: implement
+//        return false;
+//    }
+//
+//    private boolean kingValidator(ChessPiece chessPiece, Position origin, Position destination) {
+//        //TODO: implement
+//        return false;
+//    }
 
     @Override
     protected void printResult() {
