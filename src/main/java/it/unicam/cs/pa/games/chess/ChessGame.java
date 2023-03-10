3d48ec594cd46f9510614365cd1448164e30cca0
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-03-10 16:43:50 +0100

    [MINOR EDIT] remove unused code

--- src/main/java/it/unicam/cs/pa/games/chess/ChessGame.java
+++ src/main/java/it/unicam/cs/pa/games/chess/ChessGame.java
@@ -1,8 +1,6 @@
 package it.unicam.cs.pa.games.chess;
 
 import it.unicam.cs.pa.games.Game;
-import it.unicam.cs.pa.games.Position;
-
 public class ChessGame extends Game {
     private final ChessBoard board = ChessBoard.getInstance();
 
@@ -10,7 +8,6 @@ public class ChessGame extends Game {
     protected void initialize() {
         super.initialize();
         System.out.println("Inizializzazione del gioco di scacchi");
-
     }
 
     @Override
@@ -22,48 +19,6 @@ public class ChessGame extends Game {
     protected boolean endOfGame() {
         return false;
     }
-
-//    @Override
-//    protected boolean validator(ChessPiece chessPiece, Position origin, Position destination) {
-//        return switch (chessPiece.getType()) {
-//            case KING -> kingValidator(chessPiece, origin, destination);
-//            case QUEEN -> queenValidator(chessPiece, origin, destination);
-//            case ROOK -> rookValidator(chessPiece, origin, destination);
-//            case BISHOP -> bishopValidator(chessPiece, origin, destination);
-//            case KNIGHT -> knightValidator(chessPiece, origin, destination);
-//            case PAWN -> pawnValidator(chessPiece, origin, destination);
-//        };
-//    }
-//
-//    private boolean pawnValidator(ChessPiece chessPiece, Position origin, Position destination){
-//        //TODO: implement
-//        return false;
-//    }
-//
-//    private boolean knightValidator(ChessPiece chessPiece, Position origin, Position destination) {
-//        //TODO: implement
-//        return false;
-//    }
-//
-//    private boolean bishopValidator(ChessPiece chessPiece, Position origin, Position destination) {
-//        //TODO: implement
-//        return false;
-//    }
-//
-//    private boolean rookValidator(ChessPiece chessPiece, Position origin, Position destination) {
-//        //TODO: implement
-//        return false;
-//    }
-//
-//    private boolean queenValidator(ChessPiece chessPiece, Position origin, Position destination) {
-//        //TODO: implement
-//        return false;
-//    }
-//
-//    private boolean kingValidator(ChessPiece chessPiece, Position origin, Position destination) {
-//        //TODO: implement
-//        return false;
-//    }
 
     @Override
     protected void printResult() {
